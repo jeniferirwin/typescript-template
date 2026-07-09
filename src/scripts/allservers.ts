b55d8cc07e3d75c8c99b98b7dd5d92aa89efa6d9
Jenifer Irwin <jenifer.irwin@gmail.com>
2026-07-09 08:34:48 -0400

    fix: correct server access checks and improve nuke execution in startBotNet function

--- src/scripts/allservers.ts
+++ src/scripts/allservers.ts
@@ -14,19 +14,19 @@ export function startBotNet(ns: NS, hostnames: Array<string>): void {
   for (var hostname of hostnames) {
     var server = ns.getServer(hostname);
     ns.killall(hostname);
-    ns.tprint(`${hostname}`)
     if (server.purchasedByPlayer && hostname !== "home") {
       startShareScript(ns, server.hostname);
       continue;
     }
-    if (!server.hasAdminRights && canCrackPorts(ns, server.hostname)) {
+    if (!ns.hasRootAccess(hostname) && canCrackPorts(ns, hostname)) {
       crackPorts(ns, server);
-      if (canNuke(server)) {
-        ns.nuke(server.hostname);
+      if (canNuke(server) && ns.nuke(hostname)) {
+        ns.tprint(`${hostname} nuked`);
+        server = ns.getServer(hostname);
       }
     }
-    if (server.hasAdminRights && ns.getServerMaxRam(server.hostname) > 2) {
-      startShareScript(ns, server.hostname);
+    if (ns.hasRootAccess(hostname) && ns.getServerMaxRam(hostname) > 2) {
+      startShareScript(ns, hostname);
     }
     var isHackable = server.hasAdminRights && server.moneyMax !== undefined && server.moneyMax > 0 && server.requiredHackingSkill !== undefined && server.requiredHackingSkill <= ns.getPlayer().skills.hacking;
     if (isHackable === true) {
